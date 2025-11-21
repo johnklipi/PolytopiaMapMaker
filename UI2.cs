5294c61d4c1a20d17d7fb3837ee754aa5ac6367f
Fapingvin123 <fapingvin@gmail.com>
2025-11-21 10:40:09 +0100

    Rudimentary Input Box
    half-works

--- UI2.cs
+++ UI2.cs
@@ -5,8 +5,59 @@ using System.Threading.Tasks;
 using HarmonyLib;
 using UnityEngine;
 
+using TMPro;
+
 namespace PolytopiaMapManager
 {
+
+    public static class CustomInput
+    {
+        public static void AddInputToPopup(BasicPopup popup)
+        {
+            //Transform parent = popup.transform; //Popup is parent
+            var buttonContainer = popup.buttonContainer;
+            Transform parent = buttonContainer.transform.parent; //Buttons is parent
+
+            GameObject go = new GameObject("InputBox");
+            go.transform.SetParent(parent, false);
+
+            //Idk if does anything
+
+            int buttonIndex = buttonContainer.transform.GetSiblingIndex();
+            go.transform.SetSiblingIndex(buttonIndex);
+
+            // Body
+
+            RectTransform rt = go.AddComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(400, 50);
+            //rt.anchoredPosition = new Vector2(0, 150);
+
+
+            // Background
+            var bg = go.AddComponent<UnityEngine.UI.Image>();
+            bg.color = Color.white;
+
+            // Text
+            GameObject textGo = new GameObject("Text");
+            textGo.transform.SetParent(go.transform, false);
+
+            var text = textGo.AddComponent<TMPro.TextMeshProUGUI>();
+            text.text = "";
+            text.fontSize = 40;
+            text.color = Color.black;
+
+            // Text layout
+            RectTransform txtRT = textGo.GetComponent<RectTransform>();
+            txtRT.anchorMin = Vector2.zero;
+            txtRT.anchorMax = Vector2.one;
+            txtRT.offsetMin = new Vector2(10, 10);
+            txtRT.offsetMax = new Vector2(-10, -10);
+
+            // TMP InputField
+            var input = go.AddComponent<TMPro.TMP_InputField>();
+            input.textComponent = text;
+        }
+    }
     public static class UI2
     {
         private static List<string> visualMaps = new();
@@ -35,7 +86,7 @@ namespace PolytopiaMapManager
         }
         private static void OnTriedSelectDisabledMapGenType()
         {
-            NotificationManager.Notify(Localization.Get("gamesettings.nomaps", new Il2CppSystem.Object[]{}), Localization.Get("gamesettings.notavailable", new Il2CppSystem.Object[]{}), null, null);
+            NotificationManager.Notify(Localization.Get("gamesettings.nomaps", new Il2CppSystem.Object[] { }), Localization.Get("gamesettings.notavailable", new Il2CppSystem.Object[] { }), null, null);
         }
         private static void OnMapGenTypeChanged(int index)
         { }
@@ -90,19 +141,19 @@ namespace PolytopiaMapManager
             {
                 return typeof(GameSetupNameRow);
             }
-            else if(name.Contains("Spacer"))
+            else if (name.Contains("Spacer"))
             {
                 return typeof(UISpacer);
             }
-            else if(name.Contains("HorizontalList"))
+            else if (name.Contains("HorizontalList"))
             {
                 return typeof(UIHorizontalList);
             }
-            else if(name.Contains("GameSetupInfoRow"))
+            else if (name.Contains("GameSetupInfoRow"))
             {
                 return typeof(GameSetupInfoRow);
             }
-            else if(name.Contains("Button"))
+            else if (name.Contains("Button"))
             {
                 return typeof(ButtonRow);
             }
